fa82c9495b431bff330226fdfb6836262087272e
Meyjan <saragih.meyer0821@gmail.com>
2021-01-20 10:45:50 +0700

    a bit refactor to chats and page

--- src/helpers/webhookHelper.js
+++ src/helpers/webhookHelper.js
@@ -11,41 +11,16 @@ module.exports = {
 
         let response;
 
-        console.log(received_message);
-
         // Check if the message contains text
         if (received_message.text) {    
             // Create the payload for a basic text message
             response = {
                 "text": `You sent the message: "${received_message.text}". Now send me an image!`
             }
-        } else if (received_message.attachments) {
-            // Get the URL of the message attachment
-            let attachment_url = received_message.attachments[0].payload.url;
+        } else {
+            // Create the payload for a basic text message
             response = {
-                "attachment": {
-                    "type": "template",
-                    "payload": {
-                        "template_type": "generic",
-                        "elements": [{
-                            "title": "Is this the right picture?",
-                            "subtitle": "Tap a button to answer.",
-                            "image_url": attachment_url,
-                            "buttons": [
-                                {
-                                    "type": "postback",
-                                    "title": "Yes!",
-                                    "payload": "yes",
-                                },
-                                {
-                                    "type": "postback",
-                                    "title": "No!",
-                                    "payload": "no",
-                                }
-                            ],
-                        }]
-                    }
-                }
+                "text": `This chatbot doesn't handle attachment!`
             }
         } 
         
